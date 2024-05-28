eb2aed419151455c84d26bb338a811ced2509644
Drew Lee <pavlo1031@gmail.com>
2024-05-28 17:09:06 +0800

    feat: footer : add tailwind classes
    在研究如何套用 background image
    (Footer的logo、曲線圖案)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,15 @@ import './Footer.css';
 
 export function Footer() {
   return (
-    <footer className=''>
-      <div className='footer-title'></div>
-      <div className='footer-curve-line'></div>
-      <ul className='footer-text'>
-        <li><a href='javascript:void(0);'>關於我們</a></li>|
-        <li><a href='javascript:void(0);'>會員條款</a></li>|
-        <li><a href='javascript:void(0);'>隱私權政策</a></li>
+    <footer className='mt-12 py-10 gap-5 bg-grey flex flex-col justify-center items-center'>
+      <div className='footer-title text-4xl'>Jouney Bites</div>
+      <div className='footer-curve-line text-2xl'>~~~~~~</div>
+      <ul className='footer-text flex flex-row gap-4 text-2xl'>
+        <li className='font-extrabold'><a href='javascript:void(0);'>關於我們</a></li>|
+        <li className='font-extrabold'><a href='javascript:void(0);'>會員條款</a></li>|
+        <li className='font-extrabold'><a href='javascript:void(0);'>隱私權政策</a></li>
       </ul>
-      <div className='footer-copyright'>
+      <div className='footer-copyright text-base'>
         &copy;2024 Journey Bites
       </div>
     </footer>
