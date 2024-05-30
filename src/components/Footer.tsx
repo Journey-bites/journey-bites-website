55de6b6cf475ce360349677a10e7d844328f09a4
Drew Lee <pavlo1031@gmail.com>
2024-05-30 16:34:42 +0800

    feat: add event handler disabling right click menu
    • Avoid users do 'Save Image as'
      (on the footer title image and the curve line image)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,25 @@
+'use client';
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@/images/logo-lg.svg';
 import CurveLine from '@/images/footer-curve-line.svg';
 
 export function Footer() {
+
+  const onContextMenuHandler = (e) => {
+    if(e.preventDefault != undefined)
+      e.preventDefault();
+    if(e.stopPropagation != undefined)
+      e.stopPropagation();
+    return false;
+  };
+
   return (
     <footer className='mt-21 py-12 pb-13 gap-5 bg-grey flex flex-col justify-center items-center'>
-      <div className='footer-title text-4xl'>
+      <div className='footer-title text-4xl' onContextMenu={onContextMenuHandler}>
         <Image src={Logo} alt='Journey Bites' priority />
       </div>
-      <div className='footer-curve-line text-2xl'>
+      <div className='footer-curve-line text-2xl' onContextMenu={onContextMenuHandler}>
         <Image src={CurveLine} alt='' />
       </div>
       <ul className='footer-text flex flex-row gap-4 text-2xl'>
