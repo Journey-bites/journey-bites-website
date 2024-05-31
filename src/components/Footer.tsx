f07e6072555b0efff8da2b2e1ecc66a41133b2e5
Drew Lee <pavlo1031@gmail.com>
2024-06-01 05:05:53 +0800

    style: adjust width & height, margins , text sizes... for devices of each screen sizes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,13 +16,13 @@ export function Footer() {
 
   return (
     <footer className='mt-18 py-13 pb-14 gap-5 bg-grey flex flex-col justify-center items-center'>
-      <div className='footer-title text-4xl' onContextMenu={onContextMenuHandler}>
-        <Image src={Logo} alt='Journey Bites' priority />
+      <div className='footer-title' onContextMenu={onContextMenuHandler}>
+        <Image src={Logo} className='object-cover object-center w-[200px] sm:w-[240px] md:w-[240px] lg:w-[245px] xl:w-[250px]' alt='Journey Bites' priority />
       </div>
-      <div className='footer-curve-line text-2xl' onContextMenu={onContextMenuHandler}>
+      <div className='footer-curve-line mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler}>
         <Image src={CurveLine} alt='' />
       </div>
-      <ul className='footer-text flex flex-row gap-4 text-2xl'>
+      <ul className='footer-text flex flex-row gap-4 text-base sm:text-xl md:text-xl lg:text-[20px] xl:text-[21px] 2xl:text-[21px]'>
         <li className='font-extrabold'><Link href='javascript:void(0);'>關於我們</Link></li>|
         <li className='font-extrabold'><Link href='javascript:void(0);'>會員條款</Link></li>|
         <li className='font-extrabold'><Link href='javascript:void(0);'>隱私權政策</Link></li>
