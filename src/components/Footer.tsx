79b058f878d7a1cb4911e0f05d2428b98d480c0a
Drew Lee <pavlo1031@gmail.com>
2024-06-02 08:38:11 +0800

    refactor: adjust element structure.
    • remove the external wrapping <div> element of <Image>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,19 +8,15 @@ import CurveLine from '@/images/footer-curve-line.svg';
 
 export function Footer() {
 
-  const onContextMenuHandler:MouseEventHandler = (e: MouseEvent) => {
+  const onContextMenuHandler:MouseEventHandler<HTMLImageElement> = (e: MouseEvent) => {
     if(e.preventDefault != undefined)
       e.preventDefault();
   };
 
   return (
     <footer className='mt-18 py-13 pb-14 gap-5 bg-grey flex flex-col justify-center items-center'>
-      <div onContextMenu={onContextMenuHandler}>
-        <Image src={Logo} className='object-cover object-center w-[200px] sm:w-[240px] md:w-[240px] lg:w-[245px] xl:w-[250px]' alt='Journey Bites' priority />
-      </div>
-      <div className='mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler} >
-        <Image src={CurveLine} alt='' />
-      </div>
+      <Image src={Logo} className='object-cover object-center w-[200px] sm:w-[240px] md:w-[240px] lg:w-[245px] xl:w-[250px]' alt='Journey Bites' priority onContextMenu={onContextMenuHandler} />
+      <Image src={CurveLine} alt='' className='mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler} />
       <ul className='flex flex-row gap-4 text-base sm:text-xl md:text-xl lg:text-[20px] xl:text-[21px] 2xl:text-[21px]'>
         <li className='font-extrabold'><Link href='javascript:void(0);'>關於我們</Link></li>|
         <li className='font-extrabold'><Link href='javascript:void(0);'>會員條款</Link></li>|
