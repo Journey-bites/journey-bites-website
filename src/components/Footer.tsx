c20550559a2a5ebb751c927d1b75f1ff119dfdb5
Drew Lee <pavlo1031@gmail.com>
2024-06-02 09:42:32 +0800

    fix: add alt attribute values for all the <Image> tags

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,8 +14,8 @@ export function Footer() {
 
   return (
     <footer className='mt-18 py-13 pb-14 gap-5 bg-grey flex flex-col justify-center items-center'>
-      <Image src={Logo} className='object-cover object-center w-[200px] sm:w-[240px] md:w-[240px] lg:w-[245px] xl:w-[250px]' alt='Journey Bites' priority onContextMenu={onContextMenuHandler} />
-      <Image src={CurveLine} alt='' className='mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler} />
+      <Image src={Logo} alt='Journey Bites' className='object-cover object-center w-[200px] sm:w-[240px] md:w-[240px] lg:w-[245px] xl:w-[250px]' priority onContextMenu={onContextMenuHandler} />
+      <Image src={CurveLine} alt='curve' className='mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler} priority />
       <ul className='flex flex-row gap-4 text-base sm:text-xl md:text-xl lg:text-[20px] xl:text-[21px] 2xl:text-[21px]'>
         <li className='font-extrabold'><Link href='javascript:void(0);'>關於我們</Link></li>|
         <li className='font-extrabold'><Link href='javascript:void(0);'>會員條款</Link></li>|
