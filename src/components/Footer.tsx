211ac0bed231ad7a00d85213c420312deac36588
Drew Lee <pavlo1031@gmail.com>
2024-06-02 08:13:22 +0800

    refactor: change code style according  to Render Lai's suggestion.
    • Remove unnecessary classes
      (i.e. footer-title, footer-curve-line, ...)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,18 +15,18 @@ export function Footer() {
 
   return (
     <footer className='mt-18 py-13 pb-14 gap-5 bg-grey flex flex-col justify-center items-center'>
-      <div className='footer-title' onContextMenu={onContextMenuHandler}>
+      <div onContextMenu={onContextMenuHandler}>
         <Image src={Logo} className='object-cover object-center w-[200px] sm:w-[240px] md:w-[240px] lg:w-[245px] xl:w-[250px]' alt='Journey Bites' priority />
       </div>
-      <div className='footer-curve-line mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler}>
+      <div className='mt-[-6px] mb-3 lg:mb-3.5' onContextMenu={onContextMenuHandler} >
         <Image src={CurveLine} alt='' />
       </div>
-      <ul className='footer-text flex flex-row gap-4 text-base sm:text-xl md:text-xl lg:text-[20px] xl:text-[21px] 2xl:text-[21px]'>
+      <ul className='flex flex-row gap-4 text-base sm:text-xl md:text-xl lg:text-[20px] xl:text-[21px] 2xl:text-[21px]'>
         <li className='font-extrabold'><Link href='javascript:void(0);'>關於我們</Link></li>|
         <li className='font-extrabold'><Link href='javascript:void(0);'>會員條款</Link></li>|
         <li className='font-extrabold'><Link href='javascript:void(0);'>隱私權政策</Link></li>
       </ul>
-      <div className='footer-copyright text-base'>
+      <div className='text-base'>
         &copy;2024 Journey Bites
       </div>
     </footer>
