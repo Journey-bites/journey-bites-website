1b2aabcb1b15cfb45313bd04d66303f45f2f3de7
jweeei <aaa51782+wei@gmail.com>
2024-06-07 03:15:02 +0800

    fix: adjust screen in 2xl layout

--- src/app/(global)/page.tsx
+++ src/app/(global)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className='mx-auto max-w-1320'>
         <div className='grid'>
           <div className='grid grid-cols-12 md:px-3 lg:px-3 xl:px-3 2xl:px-3'>
-            <div className='2xl:col-span-8xs:mt-9 xs::mt-9 sm:col-span-12 sm:mt-9 md:col-span-12 md:mt-9 lg:col-span-8 lg:mt-20 xl:col-span-8 xl:mt-20 2xl:mt-20 xs:col-span-12'>
+            <div className='sm:col-span-12 sm:mt-9 md:col-span-12 md:mt-9 lg:col-span-8 lg:mt-20 xl:col-span-8 xl:mt-20 2xl:col-span-8 2xl:mt-20 xs:col-span-12 xs:mt-9'>
               <Content />
             </div>
             <div className='mt-20 sm:col-span-12 sm:px-3 md:col-span-12 md:ml-6 md:px-3 lg:col-span-4 lg:ml-6 xl:col-span-4 xl:ml-6 2xl:col-span-4 2xl:ml-6 xs:col-span-12 xs:px-3'>
