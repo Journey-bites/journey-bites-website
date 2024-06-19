edbe8830b4a8fda27b882ce9ca8673f05d2fd4da
hsinhui <shinhuei1@gmail.com>
2024-06-19 21:40:46 +0800

    refactor: Remove redundant component

--- src/app/(global)/(dashboard)/manage/user/page.tsx
+++ src/app/(global)/(dashboard)/manage/user/page.tsx
@@ -1,7 +1,35 @@
-import EditInfo from './EditInfo';
+'use client';
+
+import { User2Icon, Loader2Icon } from 'lucide-react';
+import TitleWIthIcon from '@/components/dashboard/TitleWIthIcon';
+import TabsWithContent from '@/components/dashboard/TabsWithContent';
+import ProfileForm from './ProfileForm';
+import LinksForm from './LinksForm';
+import { useUserStore } from '@/providers/userProvider';
 
 export default function ManageUserPage() {
+  const { auth } = useUserStore((state) => state);
+
   return (
-    <EditInfo />
+    <>
+      <TitleWIthIcon title='個人資料管理' icon={User2Icon} />
+      {!auth ? <Loader2Icon className='mx-auto size-6 animate-spin text-primary' /> : (
+        <TabsWithContent
+          defaultValue='profile'
+          tabs={[
+            {
+              value: 'profile',
+              label: '個人資料',
+              content: <ProfileForm displayName={auth.profile.displayName || ''} email={auth.email || ''} bio={auth.profile.bio || ''}/>
+            },
+            {
+              value: 'links',
+              label: '連結設定',
+              content: <LinksForm socialLinks={auth.profile.socialLinks} />
+            }]
+          }
+        />
+      )}
+    </>
   );
 }
