1f03921e33b4f04ee1219be302d234e706de3b37
hsinhui <shinhuei1@gmail.com>
2024-06-25 19:45:13 +0800

    feat: Allow all image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,28 +5,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: '**',
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
