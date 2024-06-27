f25e490f47852331c5b8bb1bc0e67796e44fdd32
hsinhui <shinhuei1@gmail.com>
2024-06-27 19:46:32 +0800

    fix: Dashboard RWD style

--- src/app/(global)/(dashboard)/manage/layout.tsx
+++ src/app/(global)/(dashboard)/manage/layout.tsx
@@ -5,7 +5,7 @@ export default function ManageLayout({ children }: PropsWithChildren) {
   return (
     <div className='mx-auto mt-6 grid max-w-[1296px] grid-cols-12 gap-6 md:mt-15'>
       <Sidebar />
-      <div className='col-span-12 px-10 md:col-span-9 '>
+      <div className='col-span-12 px-3 md:col-span-9 md:px-10 '>
         {children}
       </div>
     </div>
